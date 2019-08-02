fc8bff3bd3670392eb669598419f5f7cdf6028bc
gijslebesque <gijslebesque@gmail.com>
2019-08-02 13:20:55 +0200

    first

---
+++ javascripts/custom.js
@@ -0,0 +1,9 @@
+//when callback when DOM is ready
+$(document).ready(function() {
+  //document.getElementById("id");
+  //document.querySelector("#filter-input");
+  //document.getElementsByClassName("class");
+  //let btn = document.querySelectorAll(".square");
+  //adding eventListeners with loop;
+  //JQuery equivalents ->
+});
